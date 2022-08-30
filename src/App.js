80dc6805677c4708b602a3ae6058f2ba72341872
Boyan Velikov <boyanvelikov@gmail.com>
2022-08-30 09:11:45 +0300

    twitter and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.scss';
+import COLORS_ARRAY from './colorsArray';
 
 let quoteDBUrl = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
@@ -8,6 +9,7 @@ function App() {
   const [author, setAuthor] = useState('Will Rogers');
   const [randomNumber, setRandomNumber] = useState(0);
   const [quotesArray, setQuotesArray] = useState(null);
+  const [accentColor, setAccentColor] = useState('#282c34');
 
   const fetchQuotes = async (url) => {
     const response = await fetch(url);
@@ -22,21 +24,29 @@ function App() {
   const generateRandomQuote = ()=>{
     let randomInteger = Math.floor(quotesArray.length * Math.random());
     setRandomNumber(randomInteger);
+    setAccentColor(COLORS_ARRAY[randomInteger]);
     setQuote(quotesArray[randomInteger].quote);
     setAuthor(quotesArray[randomInteger].author);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{backgroundColor: accentColor}}>
+        <div id='quote-box' style={{color: accentColor}}>
         <h1>Random number: {randomNumber}</h1>
-        <button onClick={generateRandomQuote}>Generate a Random Quote</button>
-        <p>
+        <p id='text'>
           "{quote}"
         </p>
-        <p>
+        <p id='author'>
           - {author}
-        </p>
+        </p>   
+        <div className='button'>
+        <a id='tweet-quote' href={`http://www.twitter.com/intent/tweet?text=${quote} -${author}`} style={{backgroundColor: accentColor}}>Tweet Quote</a>
+        </div>
+
+        <button id='new-quote' onClick={generateRandomQuote} style={{backgroundColor: accentColor}}>Generate a Random Quote</button>
+        
+        </div>
       </header>
     </div>
   );
