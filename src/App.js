47601c8d16ee80b173dd5427259be7b17d25f16f
Boyan Velikov <boyanvelikov@gmail.com>
2022-08-31 08:53:44 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import './App.scss';
 import COLORS_ARRAY from './colorsArray';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 let quoteDBUrl = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
@@ -33,15 +35,15 @@ function App() {
     <div className="App">
       <header className="App-header" style={{backgroundColor: accentColor}}>
         <div id='quote-box' style={{color: accentColor}}>
-        <h1>Random number: {randomNumber}</h1>
         <p id='text'>
           "{quote}"
         </p>
         <p id='author'>
           - {author}
         </p>   
-        <div className='button'>
-        <a id='tweet-quote' href={`http://www.twitter.com/intent/tweet?text=${quote} -${author}`} style={{backgroundColor: accentColor}}>Tweet Quote</a>
+        <div className='buttons'>
+        <a id='tweet-quote' href={`http://www.twitter.com/intent/tweet?text=${quote} -${author}`} 
+        style={{backgroundColor: accentColor}}><FontAwesomeIcon icon={faTwitter} /></a>
         </div>
 
         <button id='new-quote' onClick={generateRandomQuote} style={{backgroundColor: accentColor}}>Generate a Random Quote</button>
